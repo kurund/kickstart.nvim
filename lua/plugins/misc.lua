c7476967890ac9a3b285cb24db4ccf362441abc6
Kurund Jalmi <kurund.jalmi@gmail.com>
2024-08-25 21:02:49 +0100

    add comments.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,6 +3,11 @@ return {
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- better comments
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+  },
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
